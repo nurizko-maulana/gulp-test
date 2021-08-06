2f5848fc16822f23186065213764b08734afe521
Nurizko Maulana <nurizko.maulana@gmail.com>
2021-08-07 03:46:11 +0700

    initial gulp install

---
+++ gulpfile.js
@@ -0,0 +1,13 @@
+const gulp = require(gulp);
+
+/*
+Learn gulp 
+gulp task : define tasks
+gulp.src : point to file to use 
+gulp.dest : point to folder output 
+gulp.watch : watch file and folder for changes
+*/
+
+gulp.task('defaults', function (){
+ console.log("hello this is gulp");
+});
