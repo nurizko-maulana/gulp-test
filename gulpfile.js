36ba0955da277e46aabfa7f0c9bb63546d0fff12
Nurizko Maulana <nurizko.maulana@gmail.com>
2021-08-07 06:53:43 +0700

    ADD glupfile : basic src dist and watch tools

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,32 @@
-const gulp = require(gulp);
+const {src,watch,dest,series} = require('gulp');
+const imagemin = require('gulp-imagemin');
 
 /*
 Learn gulp 
-gulp task : define tasks
-gulp.src : point to file to use 
-gulp.dest : point to folder output 
-gulp.watch : watch file and folder for changes
+
+src : point to file to use 
+dest : point to folder output 
+watch : watch file and folder for changes
 */
 
-gulp.task('defaults', function (){
- console.log("hello this is gulp");
-});
+function message(cb){
+    console.log("I'm Using Gulp");
+    cb();
+}
+
+function copyHtml(){
+    return src('src/*.html').pipe(dest('dist'))
+}
+
+function build(cb){
+    series(message,build,watchFile);
+    cb();
+}
+
+function watchFile(){
+    watch('src/*.html', copyHtml)
+}
+
+exports.build = build;
+exports.default = watchFile;
+
